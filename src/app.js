81b9122fbff8af800de163e2dc6fd16f1affe635
konradkam <konradkam@gmail.com>
2019-02-04 12:03:20 +0100

    Add animated background

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 require('./styles.scss');
 
 import SmoothScroll from "./vendor/smooth-scroll/smooth-scroll.js";
+import Two from "../node_modules/two.js/build/two.js";
 import AOS from 'aos';
 
 document.addEventListener('DOMContentLoaded', () => {
+    // Logo Animation
     const letters = document.getElementsByClassName('letter');
     const caret = document.getElementById('caret');
 
@@ -62,14 +64,15 @@ document.addEventListener('DOMContentLoaded', () => {
         }, (Math.random() * 250))
     }
 
+    setTimeout(function () {
+        myLoop();
+    }, 1300);
+
+    //Setup of Animate On Scroll Library
     AOS.init({
         once: 'true',
         disable: 'phone',
     });
-
-    setTimeout(function () {
-        myLoop();
-    }, 1300);
 
     // Scroll to anchor
     let scroll = new SmoothScroll('a[href*="#"]', {
@@ -78,4 +81,56 @@ document.addEventListener('DOMContentLoaded', () => {
         speed: 300
     });
 
+    // Backgroud animation
+    const box = document.getElementById('section-two');
+
+    let boxWidth = box.clientWidth;
+    let boxHeight = box.clientHeight;
+
+    const toRadian = Math.PI / 180;
+    function angleInRadians(a) {
+        return a * toRadian;
+    }
+    let angle = 0;
+
+    const points = [
+        new Two.Vector(0, boxHeight),
+        new Two.Vector(boxWidth, boxHeight),
+        new Two.Vector(boxWidth, 0),
+        new Two.Vector(boxWidth * 0.75, boxHeight/2),
+        new Two.Vector(boxWidth * 0.5, boxHeight * 0.5),
+        new Two.Vector(boxWidth * 0.25, 60)
+    ];
+
+    const elem = document.getElementById('two');
+    const params = {
+        type: Two.Types.svg,
+        width: boxWidth,
+        height: boxHeight };
+    const two = new Two(params).appendTo(elem);
+
+    const path =  two.makePath(points, close);
+
+    path.fill = '#f1f1f1';
+    path.noStroke();
+    path.translation.set = (0, 0);
+
+    function move(point, offset, in_min, in_max, out_min, out_max) {
+        if (angle > 360) {
+            angle = 0;
+        }
+        point.y = (Math.sin(angleInRadians(angle + offset)) - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        angle += 0.25;
+    }
+
+    two.bind('update', function(frameCount) {
+        move(points[3], 0, -1, 1, boxHeight/2.5 * -1, boxHeight/7);
+        move(points[5], 50, -1, 1, boxHeight/7 * -1, boxHeight/2.5);
+    }).play();
+
+    //Reloading page to update background animation on screen resize
+    window.addEventListener("resize", function(event)
+    {
+        location.reload();
+    });
 });
