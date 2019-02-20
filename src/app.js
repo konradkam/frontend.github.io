e6fb3482dd1368957abc58e8a0d44294a180f261
konradkam <konradkam@gmail.com>
2019-02-20 14:19:05 +0100

    Refactor to simplify typingAnimation function

--- src/app.js
+++ src/app.js
@@ -9,63 +9,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const letters = document.getElementsByClassName('letter');
     const caret = document.getElementById('caret');
 
+    const caretPositions = [-167, -156, -145, -133, -118, -95, -82, -70, -56, -45, -33, -22, -11, 0];
     let i = 0;
 
-    function myLoop () {
-        setTimeout(function () {
+    function typingAnimation() {
+        setTimeout(function() {
             letters[i].style.opacity = 1;
+            caret.setAttribute('transform','translate(' + caretPositions[i] + ', 0)');
             i++;
-            switch (i) {
-                case 1:
-                    caret.setAttribute('transform','translate(-167, 0)');
-                    break;
-                case 2:
-                    caret.setAttribute('transform','translate(-156, 0)');
-                    break;
-                case 3:
-                    caret.setAttribute('transform','translate(-145, 0)');
-                    break;
-                case 4:
-                    caret.setAttribute('transform','translate(-133, 0)');
-                    break;
-                case 5:
-                    caret.setAttribute('transform','translate(-118, 0)');
-                    break;
-                case 6:
-                    caret.setAttribute('transform','translate(-95, 0)');
-                    break;
-                case 7:
-                    caret.setAttribute('transform','translate(-82, 0)');
-                    break;
-                case 8:
-                    caret.setAttribute('transform','translate(-70, 0)');
-                    break;
-                case 9:
-                    caret.setAttribute('transform','translate(-56, 0)');
-                    break;
-                case 10:
-                    caret.setAttribute('transform','translate(-45, 0)');
-                    break;
-                case 11:
-                    caret.setAttribute('transform','translate(-33, 0)');
-                    break;
-                case 12:
-                    caret.setAttribute('transform','translate(-22, 0)');
-                    break;
-                case 13:
-                    caret.setAttribute('transform','translate(-11, 0)');
-                    break;
-                default:
-                    caret.setAttribute('transform','translate(0, 0)');
-            }
             if (i < letters.length) {
-                myLoop();
+                typingAnimation();
             }
         }, (Math.random() * 250))
     }
 
     setTimeout(function () {
-        myLoop();
+        typingAnimation();
     }, 1300);
 
     //Setup of Animate On Scroll Library
